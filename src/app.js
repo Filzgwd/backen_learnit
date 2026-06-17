bb30300138a690b3bc404777d89e16c63db58a3b
23091397082_M Afilla Riyan <149144961+Filzgwd@users.noreply.github.com>
2026-06-18 03:37:14 +0700

    Add learnit-id.vercel.app to CORS origins

--- src/app.js
+++ src/app.js
@@ -8,7 +8,8 @@ app.use(cors({
   origin: [
     'http://localhost:5173',
     'http://localhost:3000',
-    'https://learnit.vercel.app'
+    'https://learnit.vercel.app',
+    'https://learnit-id.vercel.app'
   ],
   credentials: true
 }));
